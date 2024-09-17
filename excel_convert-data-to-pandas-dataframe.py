5c9aec5025880261a9ba3c6eef9cffd65578e84f
Pierre P <95609842+CourteousTuna@users.noreply.github.com>
2024-09-17 16:25:54 +0200

    Update excel_convert-data-to-pandas-dataframe.py

--- excel_convert-data-to-pandas-dataframe.py
+++ excel_convert-data-to-pandas-dataframe.py
@@ -8,20 +8,20 @@ df = pd.read_csv(file_path) # For .xlsx use read_excel() instead
 # Converts to string and removes last 4 characters
 df['id_demande'] = df['id_demande'].astype(str).str[:-4]
 
-# Slicing data frame in list
+# Slicing data frame into a list
 ids = df['id_demande'][:len].values
 
-# Filtering df
+# Filtering the df
 df = df[df['type_demande'] == "PREST ADH"]
 filtered_df = df[df['type_document_detaille'].isin(["FACTURE", "BULLETIN HOSPI VERST IJ"])]
 
-# Group by unique combinations of id_demande and type_document_detaille 
+# Groups by unique combinations of id_demande and type_document_detaille 
 df_grouped = filtered_df.groupby('id_demande')['type_document_detaille'].nunique()
 
 # Counts the total number of rows with distinct id_demande
 total_distinct_ids =  df[df['type_document_detaille'] == "FACTURE"]['id_demande'].nunique()
 
-# Filter df by rows that have 2 rows with the same id_demande and distinct type_document_detaille values 
+# Filters df by rows that have 2 rows with the same id_demande and distinct type_document_detaille values 
 df_with_both = df_grouped[df_grouped == 2].index.tolist()
 
 # Average number of lines per id_demande
